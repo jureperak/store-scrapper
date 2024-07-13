fde0a8ff7fe8fea6bdd3ef2a6fd147949656e452
Jure Perak <jure.perak@mober.hr>
2024-07-13 12:20:25 +0200

    inital page

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const fontHeading = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-heading",
+});
+
+const fontBody = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-body",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn("antialiased", fontHeading.variable, fontBody.variable)}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
