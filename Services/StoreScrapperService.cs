18d98f56c2150d6ecb20119ad9c37451e681b34c
Jure Perak <jure.perak@moberg.hr>
2026-01-08 23:43:23 +0100

    .net 9

--- Services/StoreScrapperService.cs
+++ Services/StoreScrapperService.cs
@@ -22,17 +22,13 @@ public class StoreScrapperService
 
         while (continueCrawling)
         {
-            continueCrawling = await ScrapeStore(
-                _zaraAdapter,
-                "https://www.zara.com/itxrest/1/catalog/store/11737/product/id/316571537/availability?ajax=true",
-                "https://www.zara.com/hr/hr/kratka-jakna-od-umjetnog-krzna-p06318261.html?v1=316571537&utm_campaign=productShare&utm_medium=mobile_sharing_iOS&utm_source=red_social_movil"
-            );
-
             if (continueCrawling)
             {
                 Console.WriteLine($"Request #{numberOfRequests++}");
                 Thread.Sleep(4000);
             }
+            
+            continueCrawling = false;
         }
     }
 
