96dfc22f3e68772d25e08efe7cadeabc6913c58d
Jure Perak <jure.perak@moberg.hr>
2026-01-11 12:51:19 +0100

    seed

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,12 +10,33 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<Adapter> Adapters { get; set; }
-    
+
     public DbSet<Product> Products { get; set; }
-    
+
     public DbSet<ProductSku> ProductSkus { get; set; }
-    
+
     public DbSet<NotificationHistory> NotificationHistory { get; set; }
-    
+
     public DbSet<JobExecutionLog> JobExecutionLogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Seed Adapter data
+        modelBuilder.Entity<Adapter>().HasData(
+            new Adapter
+            {
+                Id = 1,
+                Name = "Zara",
+                CreatedAt = new DateTime(2026, 1, 11, 0, 0, 0, DateTimeKind.Utc)
+            },
+            new Adapter
+            {
+                Id = 2,
+                Name = "PullAndBear",
+                CreatedAt = new DateTime(2026, 1, 11, 0, 0, 0, DateTimeKind.Utc)
+            }
+        );
+    }
 }
